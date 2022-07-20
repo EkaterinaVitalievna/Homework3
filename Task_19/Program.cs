0ff2e232790d06912053609f2c2a237c0b9f9621
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-20 23:30:21 +0300

    Является ли число палиндромом

---
+++ Task_19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число");
+string numbers = Console.ReadLine();
+string palindrom()
+{
+    if (numbers.Length == 5)
+    {
+        if (numbers[0] == numbers[4] && numbers[1] == numbers[3])
+        {
+            return "Введенное число является палиндромом";
+        }
+        else
+        {
+            return "Введенное число не является палиндром";
+        }
+    }
+    else
+    {
+        return "Введите пятизначное число!";
+    }
+}
+
+Console.WriteLine(palindrom());
